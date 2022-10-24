40315502d8621c52105317c72e082e350cd27313
GabrielAndrade <gabrieolandrade@hotmail.com.br>
2022-10-23 22:39:39 -0300

    commit 4

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,10 @@
-import { Button, Container, Flex } from "@chakra-ui/react";
+import { Button, Container, Flex, Image, Spacer, Text } from "@chakra-ui/react";
 import { auth, db, logout } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { useCallback, useEffect, useState } from "react";
+import { ImInfinite } from "react-icons/im";
+import { AiFillStar } from "react-icons/ai";
 import { AiOutlinePlus } from "react-icons/ai";
 import NotePreview from "../components/NotePreview";
 import NoteModal from "../components/NoteModal";
@@ -12,10 +14,15 @@ import { uuidv4 } from "@firebase/util";
 export default function Dashboard() {
   const [user, loading] = useAuthState(auth);
   const [notes, setNotes] = useState([]);
-
-  const [modalNoteContent, setModalNoteContent] = useState(undefined);
+  const [showFavoriteOnly, setShowFavoriteOnly] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
+  const [modalNoteContent, setModalNoteContent] = useState(undefined);
   const navigate = useNavigate();
+
+  const favoritedNotes = notes.filter((note) => note.isFavorite);
+  const notesToShow = showFavoriteOnly ? favoritedNotes : notes;
+
+
 
   useEffect(() => {
     if (loading) return;
@@ -34,6 +41,11 @@ export default function Dashboard() {
     fetchData();
   }, [fetchData]);
 
+  useEffect(() => {
+    if (modalNoteContent?.temp) {
+      setModalNoteContent(undefined);
+    }
+  }, [modalNoteContent])
 
   async function createNote(noteContent) {
     const id = uuidv4();
@@ -68,6 +80,15 @@ export default function Dashboard() {
     setModalOpen(true);
   }
 
+  function openModalForCreatingNote() {
+    setModalNoteContent(undefined);
+    setModalOpen(true);
+  }
+
+  function closeModal() {
+    setModalNoteContent({ title: "", content: "", isFavorite: false, temp: true });
+    setModalOpen(false);
+  }
 
   function toggleFavorite(noteContent) {
     updateNote({
@@ -76,29 +97,33 @@ export default function Dashboard() {
     });
   }
 
-  function openModalForCreatingNote() {
-    setModalNoteContent(undefined);
-    setModalOpen(true);
-  }
-  
-  function closeModal() {
-    setModalNoteContent({ title: "", content: "", isFavorite: false, temp: true });
-    setModalOpen(false);
-  }
-  
   return (
     <>
       <Flex width="100vw" height="100vh">
+        <Flex width="320px" height="100%" shadow="lg" zIndex={2} direction="column" pt="10px" pb="20px">
+          <Container>
+            <Flex align="center">
+              <Image src="book-logo.png" alt="Logo" boxSize="70px" />
+              <Text>{user?.displayName}</Text>
+            </Flex>
+          </Container>
+          <Container p={0}>
+            <Button colorScheme="orange" variant="ghost" size="lg" justify="flex-start" w="100%" mt="20px" leftIcon={<ImInfinite />} onClick={() => setShowFavoriteOnly(false)}>Todas as notas</Button>
+            <Button colorScheme="orange" variant="ghost" size="lg" justify="flex-start" w="100%" mt="0px" leftIcon={<AiFillStar />} onClick={() => setShowFavoriteOnly(true)}>Favoritas</Button>
+          </Container>
+          <Spacer />
+          <Container>
+            <Button variant="solid" bg="red" color="white" onClick={logout} width="100%">Sair</Button>
+          </Container>
+        </Flex>
         <Container bg="gray.200" maxWidth="100vw" minHeight="100vh" p="20px" >
           <Button variant="outline" color="gray.700" borderColor="gray.700" borderWidth="2px" onClick={openModalForCreatingNote} rightIcon={<AiOutlinePlus />}>Nova nota</Button>
           <Flex flexWrap="wrap">
-            {notes?.map((noteContent) => <NotePreview noteContent={noteContent} toggleFavorite={() => toggleFavorite(noteContent)} onClick={() => openModalForEditingNote(noteContent)} />)}
+            {notesToShow?.map((noteContent) => <NotePreview noteContent={noteContent} toggleFavorite={() => toggleFavorite(noteContent)} onClick={() => openModalForEditingNote(noteContent)} />)}
           </Flex>
         </Container>
       </Flex>
-      <Container>
-        <Button variant="solid" bg="red" color="white" onClick={logout} width="100%">Sair</Button>
-      </Container>
+
       <NoteModal noteContent={modalNoteContent === undefined ? undefined : modalNoteContent} isOpen={modalOpen} onClose={closeModal} onSubmit={modalNoteContent ? updateNote : createNote} />
     </>
   );
